514590c1f8f9e79c70eae26499d402cd12ba4f6c
carlos <cds11@aluno.ifal.edu.br>
2025-11-04 13:50:10 -0300

    quiz intro improved

--- lib/pages/GlossarioPage.dart
+++ lib/pages/GlossarioPage.dart
@@ -16,7 +16,7 @@ class _GlossarioPageState extends State<GlossarioPage> {
   @override
   void initState() {
     super.initState();
-    futurePropriedades = PropriedadesApi().findAll(); // aqui funciona
+    futurePropriedades = PropriedadesApi().findAll();
   }
 
   @override
